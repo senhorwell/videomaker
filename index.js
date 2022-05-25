44e2594395ca3f91c7ce03a5fca9f9e796afdd02
Well Luiz <wellington.luiz@bldlogistica.com.br>
2022-05-24 23:45:06 -0300

    instalando rss-parser e fazendo a pesquisa do searchTerm nos trendingsearchs do google

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const readline = require('readline-sync')
+const Parser = require('rss-parser');
 
-function start() {
+const TREND_URL = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=BR'
+
+async function start() {
     const content = {}
 
-    content.searchTerm = askAndReturnSearchTerm()
+    content.searchTerm = await askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
 
-    function askAndReturnSearchTerm() {
-        return readline.question('Type a Wikipedia search term: ')
+    async function askAndReturnSearchTerm() {
+        return await askAndReturnTrend()
+    }
+
+    async function askAndReturnTrend() {
+        const trends = await getGoogleTrends()
+        const choice = readline.keyInSelect(trends, 'Choose your trend:')
+
+        return trends[choice]
+
+    }
+
+    async function getGoogleTrends() {
+        const parser = new Parser();
+        const trends = await parser.parseURL(TREND_URL);
+        return trends.items.map(i => i.title)
     }
 
     function askAndReturnPrefix() {
         const prefixes = ['Who is', 'What is', 'The history of']
-        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Chose one option: ')
-        return prefixes[selectedPrefixIndex]
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
+        const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+        return selectedPrefixText
     }
 
     console.log(content)
