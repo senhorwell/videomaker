e2aaa67a4fec29afdc58d3b265e421e7d83babe4
Well Luiz <wellington.luiz@bldlogistica.com.br>
2022-05-24 23:37:13 -0300

    Inicial do projeto node com inputs simples de searchterm e prefixos

---
+++ index.js
@@ -0,0 +1,22 @@
+const readline = require('readline-sync')
+
+function start() {
+    const content = {}
+
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+        return readline.question('Type a Wikipedia search term: ')
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Chose one option: ')
+        return prefixes[selectedPrefixIndex]
+    }
+
+    console.log(content)
+}
+
+start()
